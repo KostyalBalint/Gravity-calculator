4949645c0484852ded025576343ac69d910f83f6
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-01-15 17:13:50 +0100

    App entry point changed to ./dist to ./ for GithubPages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
   },
   watch: true,
 };
